918dc9bbf67b672b6e57d29cc644d17f7c93634c
happyhar <65075165+happyhar@users.noreply.github.com>
2020-07-15 18:37:04 +0530

    Day 1 Assign

---
+++ Day 1/intro.js
@@ -0,0 +1,12 @@
+var a = {name: "hartsjha",age:"353",col:"black"}
+var b = {name: "rtt",age:"353",col:"black"}
+var c = {name: "efef",age:"353",col:"black"}
+console.log("HELLO ARE U READY")
+console.error("Congrats u got an error");
+console.warn("congrats u got a warning");
+// console.clear();
+console.time('TOTAL TIME TAKEN:');
+
+console.table({a,b,c})
+
+console.timeEnd('TOTAL TIME TAKEN:');
