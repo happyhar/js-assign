f9802d44272cf67cf18bc346190620bd877613e9
happyhar <65075165+happyhar@users.noreply.github.com>
2020-07-16 12:37:14 +0530

    Assignment 2

---
+++ DAY 2/script.js
@@ -0,0 +1,47 @@
+// a = prompt("whats is ur name");
+// console.log("HEY " + a);
+// string concatenation
+// let str1 = " HEY";
+// let str2 = " I'M BACK";
+// var res = str1.concat(str2);
+// console.log(res);
+
+// String match
+// letyt str = "The rain in SPAIN stays mainly in the plain";
+// let res = str.match(/ain/g);
+// console.log(res);
+
+// String Search
+// var str = "LetsUpgrade Free Coding School";
+// var n = str.search("Free");
+// console.log(n)
+
+// String Slice
+// var str = "HEY THERE";
+// var res = str.slice(0, 3);
+// console.log(res);
+
+
+// ARRAY
+
+// Concatenate
+// let a = ["Banana", "mango"];
+// let b = ["Carrot", "Raddish"];
+// let combo = a.concat(b);
+// console.log(combo);
+
+//Push
+// let a = ["hi","hey"];
+// console.log(a);
+// a.push("hello")
+// console.log(a);
+
+// Pop
+// let a = ["hi", "hey", "hello"]
+// a.pop();
+// console.log(a);
+
+// Reverse
+let a = ["hi", "hey", "hello"];
+a.reverse();
+console.log(a);
