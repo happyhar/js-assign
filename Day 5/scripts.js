bced78f6394d0dcfcc9fb97a20f53841a3663c79
happyhar <65075165+happyhar@users.noreply.github.com>
2020-07-21 14:28:24 +0530

    Day 5 Assignment

---
+++ Day 5/scripts.js
@@ -0,0 +1,16 @@
+console.log("Lets get started ");
+// Q1
+
+
+let n =prompt("enter a number");
+        console.log(Math.abs(n));
+
+let arr=[n];
+        arr.forEach((el)=>{
+            console.log(arr)
+        })
+
+        let odd=arr.filter(el=>el%2!=0);
+        console.log(odd);
+
+
